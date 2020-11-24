3e8415bbc8e7260cad926da3baac41dcd192c95e
Khaled Omar <omark01@pfw.edu>
2020-11-24 16:28:41 -0500

    Added StockAPI and ETFAPI

---
+++ SNZY.Models/APIModels/StockAPIModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNZY.Models.APIModels
+{
+    public class StockAPIModel
+    {
+        public class Meta
+        {
+            public string symbol { get; set; }
+        }
+
+        public class Value
+        {
+            public string datetime { get; set; }
+            public string open { get; set; }
+            public string high { get; set; }
+            public string low { get; set; }
+            public string close { get; set; }
+            public string volume { get; set; }
+        }
+        public Meta meta { get; set; }
+        public List<Value> values { get; set; }
+        public string status { get; set; }
+    }
+}
