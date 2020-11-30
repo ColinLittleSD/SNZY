327f0b60af25c4b334959c2b333036aab6c08128
ColinLittleSD <ColinLittleSD@gmail.com>
2020-11-30 09:52:50 -0500

    fixed merge conflict

--- SNZY.Models/StockWithNewData.cs
+++ SNZY.Models/StockWithNewData.cs
@@ -11,23 +11,13 @@ namespace SNZY.Models
         public string StockName { get; set; }
         public string Ticker { get; set; }
         public double Price { get; set; }
-<<<<<<< HEAD
         public double MarketCap { get; set; }
-=======
-        //public double AverageVolume { get; set; }
-        public double MarketCap { get; set; }
-
->>>>>>> 3245d8c1ceb46b79881545a5922bf67530fc67c6
         public DateTime Datetime { get; set; }
         public double Open { get; set; }
         public double High { get; set; }
         public double Low { get; set; }
         public double Close { get; set; }
         public double Volume { get; set; }
-<<<<<<< HEAD
-=======
-
->>>>>>> 3245d8c1ceb46b79881545a5922bf67530fc67c6
         public string BuyOrHold { get; set; }
     }
 }
