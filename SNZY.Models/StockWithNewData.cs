eddc301a71d8338e9ebd5615568a57bc5144e02b
Max Sigmon <max.j.sigmon@gmail.com>
2020-11-30 09:42:43 -0500

    removed AverageVolume prop

---
+++ SNZY.Models/StockWithNewData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNZY.Models
+{
+    public class StockWithNewData
+    {
+        public string StockName { get; set; }
+        public string Ticker { get; set; }
+        public double Price { get; set; }
+        public double MarketCap { get; set; }
+        public DateTime Datetime { get; set; }
+        public double Open { get; set; }
+        public double High { get; set; }
+        public double Low { get; set; }
+        public double Close { get; set; }
+        public double Volume { get; set; }
+        public string BuyOrHold { get; set; }
+    }
+}
